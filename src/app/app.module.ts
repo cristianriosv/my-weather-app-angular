b75bcd88be9212729433dc3c6625684ed8facc8a
Cristian Rios <cristianriosv@gmail.com>
2023-10-05 15:42:50 +0200

    feat: add general layout for content, footer and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { FooterLayoutComponent } from './layout/footer-layout/footer-layout.component';
+import { HeaderLayoutComponent } from './layout/header-layout/header-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentLayoutComponent,
+    FooterLayoutComponent,
+    HeaderLayoutComponent
   ],
   imports: [
     BrowserModule,
